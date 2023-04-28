7ba9c77bdc7305454f410cca09b15546c995de1c
The Duz <tamara.dusenbury@gmail.com>
2023-04-27 23:07:46 -0700

    attempt to debug tables. Tables laughed.

--- db/schema.sql
+++ db/schema.sql
@@ -4,25 +4,26 @@ CREATE DATABASE tracker_db;
 USE tracker_db;
 
 CREATE TABLE departments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    dept_names VARCHAR(30)
+    id INT AUTO_INCREMENT,
+    dept_names VARCHAR(30),
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE job_titles (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
     dept_id INT,
+    PRIMARY KEY (id),
     FOREIGN KEY (dept_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    FOREIGN KEY (role_id) REFERENCES job_titles(id),
-   /* manager_id INT NULL,
-    FOREIGN KEY (manager_id) REFERENCES employees(id)*/
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles (id)
 );
 
